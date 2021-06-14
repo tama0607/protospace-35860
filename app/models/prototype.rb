195922b09a428b7a911264fa28d96e6a16ee9eab
yukihirasrr <rito.yukihira1111@gmail.com>
2021-06-14 09:39:07 +0900

    image_magick,mini_magicの導入、new,createコントローラ追加、prototypeアソシエーションバリデーション追加

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -4,6 +4,8 @@ class Prototype < ApplicationRecord
   validates :catch_copy, presence:true
   validates :concept, presence:true
   validates :user, presence:true
+  validates :image, presence: true
 
   belong_to :user
+  has_one_attached :image
 end
