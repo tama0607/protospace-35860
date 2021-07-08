075b1c80d3a7cff072fa1946008cca94b3a67f2b
yukihirasrr <rito.yukihira1111@gmail.com>
2021-07-08 09:30:00 +0900

    投稿機能の実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,11 +1,23 @@
 class PrototypesController < ApplicationController
+  
   def index
   end
 
   def new
+    @prototype = Prototype.new
   end
 
   def create
+    @prototype = Prototype.new(prototype_form)
+    if @prototype.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
+  private
+  def prototype_form
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
 end
